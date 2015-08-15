7161c9ac1a62672035a4c8c6d26cb00cf884dd7c
pagrawl3 <mail@prath.am>
2015-08-16 00:08:27 +0530

    more content

--- assets/jsx/components/global/menu.jsx
+++ assets/jsx/components/global/menu.jsx
@@ -5,9 +5,9 @@ export class Menu extends React.Component {
 		return (
 			<div className="menu-wrapper">
 				<ul className="menu">
-					<li className="menu-item">About</li>
-					<li className="menu-item">Our Leadership</li>
-					<li className="menu-item">Projects</li>
+					<li className="menu-item selected home">INDIA STORIES</li>
+					<li className="menu-item leadership">Leadership</li>
+					<li className="menu-item projects">Projects</li>
 				</ul>
 
 			</div>
--- assets/jsx/pages/home.jsx
+++ assets/jsx/pages/home.jsx
@@ -16,77 +16,69 @@ class Home extends React.Component{
 				</section>
 				<section className="about-us">
 				 	<h2> OUR STORY </h2>
+				 	<hr/>
 				 	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
 
 					"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
 					Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
 				</section>
+
 				<section className="our-philosophy">
 					<h2> OUR PHILOSOPHY</h2>
-					<div className="mini-section-1">
-						<Svg_up svg="Brain" className="section-1"/>
-						<div className=" brain-text section-1" >
-							<h2>Systemic Thinking</h2>
+					<hr/>
+					<article className="mini-section">
+						<Svg_up svg="Brain" className="svg-icon"/>
+						<div className="text-wrapper" >
+							<h3>Systemic Thinking</h3>
 							<p>All learnings will be institutionalized. 
 								Orientation towards long term value v/s short term profit.
 								Continuous Disruptive innovation.</p>
 						</div>
-
-					</div>
-					<div className="mini-section-1">
-						<Svg_up svg="Personal-Mastery" className="section-1"/>
-						<div className="brain-text section-1">
-							<h2>Personal Mastery</h2>
-							<p>Core Competence. 
-								Creative tension – aspiring to reach ideals. 
-								Openness and adaptability</p>
-						</div>
-
-					</div>
-					<div className="mini-section-1">
-						<Svg_up svg="Leadership" className="section-1"/>
-						<div className="brain-text section-1">
-							<h2>Leadership</h2>
-							<p>Shared Vision.
-							Self Directed Teams.
-						Leaders as merely custodians of creative tension</p>
-						</div>
-
-					</div>
-					<div className="mini-section-1">
-						<Svg_up svg="Compass" className="section-1"/>
-						<div className="brain-text section-1">
-							<h2>Core Values</h2>
+					</article>
+					<article className="mini-section">
+						<Svg_up svg="Compass" className="svg-icon"/>
+						<div className="text-wrapper">
+							<h3>Core Values</h3>
 							<p>Entertainment  using the power of imagination.
 								Undistorted  transmission of culture and objective truths.
 								No compromise on ethics and morality.
 								Prudent transparency at all levels.</p>
 						</div>
-
-					</div>
+					</article>
+					<article className="mini-section">
+						<Svg_up svg="Personal-Mastery" className="svg-icon"/>
+						<div className="text-wrapper">
+							<h3>Personal Mastery</h3>
+							<p>Core Competence. 
+								Creative tension – aspiring to reach ideals. 
+								Openness and adaptability</p>
+						</div>
+					</article>
+					<article className="mini-section">
+						<Svg_up svg="Leadership" className="svg-icon"/>
+						<div className="text-wrapper">
+							<h3>Leadership</h3>
+							<p>Shared Vision.
+								Self Directed Teams.
+								Leaders as merely custodians of creative tension</p>
+						</div>
+					</article>
 				</section>
-				<section className="Vision">
-					<h2>VISION</h2>
-					<p>To be the leading producer of entertainment content from India  for domestic and global audiences.</p>
 
-				</section>
-
-				<section className="Mission">
-					<h2>MISSION</h2>
-					<p> To tell great stories and thereby humanize life.</p>
-
-				</section>
-
-				<section className="Propostion">
-					<h2>VALUE PROPOSITION</h2>
-					<p>High credibility and access in the Industry.
-
-					Perfect match between creative vision  and business wisdom.
-
-					Optimally designed product portfolio to balance risk and return.
-
-					Veteran team of professional managers poised to capture the growth when the industry is seeing an inflexion point.</p>
-
+				<section className="bg-image-grid">
+					<article className="vision">
+						<h2>VISION</h2>
+						<p>To be the leading producer of entertainment content from India  for domestic and global audiences.</p>
+					</article>
+					<article className="mission">
+						<h2>MISSION</h2>
+						<p> To tell great stories and thereby humanize life.</p>
+					</article>
+					<article className="proposition">
+						<h2>VALUE PROPOSITION</h2>
+						<p>High credibility and access in the Industry.
+						Perfect match between creative vision  and business wisdom. Optimally designed product portfolio to balance risk and return. Veteran team of professional managers poised to capture the growth when the industry is seeing an inflexion point.</p>
+					</article>
 				</section>
 			</div>
 		)
