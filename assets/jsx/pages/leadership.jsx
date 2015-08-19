3846904039ec6a291f1d5edfdace73377b605e77
sim13 <simrinwahal@gmail.com>
2015-08-20 00:47:51 +0530

    Added leadership page

--- assets/jsx/pages/leadership.jsx
+++ assets/jsx/pages/leadership.jsx
@@ -10,7 +10,24 @@ export class Leadership extends React.Component{
 	render() {
 		return (
 			<div>
-				<h1> LEADERSHIP </h1>
+				<section className="leadership">
+				<div className="image-wrapper">
+					<div className="image"></div>
+				</div>
+					<h1> LEADERSHIP </h1>
+					<article className="team-members">
+						<div className="member-1 member">
+							<h2>K.R. Harish</h2>
+							<p>A start up specialist with experience of more than 20 years in media industry spanning  Satellite Television, Satellite Radio, Music Record Label and Motion Pictures.
+Past assignments included Dy.CFO of Star TV, Managing Director and CFO Asia Pacific World Space, Group CFO Data Access, Managing Director Phat Phish India P Limited and Producer at Lightforms and Pictures Pvt Ltd</p>
+						</div>
+						<div className="member-2 member">
+							<h2> Trilok Malhotra</h2>
+							<p>An industry veteran having spent over 28 years in the industry covering the entire life cycle of film production management, starting from validation of scripting, budgeting, production, marketing and distribution of films. Has managed shoots in various locations of India, and overseas.Was until recently the Head of Production of Fox Star Studios, specifically hired by Fox to lead their foray into Bollywood. </p>
+						</div>
+
+					</article>
+				</section>
 			</div>
 		)
 	}
